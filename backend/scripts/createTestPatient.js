824f86b8b1e5097e4f9fcd10eb525c0172d8dd2b
dncoder14 <dhirajpandit5050@gmail.com>
2025-12-02 21:24:07 +0530

    Fix all Prisma client imports

--- backend/scripts/createTestPatient.js
+++ backend/scripts/createTestPatient.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('../generated/prisma');
+const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
 
 const prisma = new PrismaClient();
